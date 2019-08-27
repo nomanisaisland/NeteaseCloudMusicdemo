3c3f2617cb644767651af0a32026c9dd5fa40a65
nomanisaisland <934789537@qq.com>
2019-08-27 09:47:15 +0800

    most function

--- NeteaseCloudMusicdemo/script/homepage/index_homepage.js
+++ NeteaseCloudMusicdemo/script/homepage/index_homepage.js
@@ -1,5 +1,3 @@
-
-
 // 寻找内容    歌单推荐
 render("http://localhost:3000/personalized").then(data => {
     addHtml($(".recommend-titile-header"), data.result.splice(0, 6), function callback(link, value, index) {
@@ -22,7 +20,7 @@ render("http://localhost:3000/personalized").then(data => {
 })
 // 推荐歌单新歌渲染
 render("http://localhost:3000/personalized/newsong").then(data => {
-    addHtml($(".song-list-content"), data.result, function callback(link, value,index) {
+    addHtml($(".song-list-content"), data.result, function callback(link, value, index) {
         return input =
             `
             <a href=${link+value.id+"?pic="+value.song.album.blurPicUrl+"?cls=10000"}>
@@ -92,7 +90,7 @@ render("http://localhost:3000/top/list?idx=1").then(data => {
 
 render("http://localhost:3000/search/hot").then(data => {
 
-    addHtml($("#tab_history"), data.result.hots, function callback(link,value,index) {
+    addHtml($("#tab_history"), data.result.hots, function callback(link, value, index) {
         return input =
             `
             <li class="search_tap">
@@ -130,6 +128,7 @@ function addSearHis() {
 // 数组清空
 // 渲染本地存储的数组格式数据
 makeArr();
+
 function makeArr() {
     if (data_arr !== null) {
         // console.log(data_arr);
@@ -154,6 +153,7 @@ function makeArr() {
 }
 // 删除按钮
 removeSearHis()
+
 function removeSearHis() {
     let data_arr = JSON.parse(window.localStorage.getItem("kserch_data_hisey"));
     for (let i = 0; i < $A(".remove").length; i++) {
@@ -169,9 +169,9 @@ function removeSearHis() {
 }
 
 //热歌 排行前三红色，后面#999
-function addColor(){
-    $A(".song-list-content-left").forEach(value=>{
-        if(value.innerHTML > 3){
+function addColor() {
+    $A(".song-list-content-left").forEach(value => {
+        if (value.innerHTML > 3) {
             value.style.color = "#999";
         }
     })
@@ -179,6 +179,87 @@ function addColor(){
 
 
 // 登录按钮悬浮效果功能
-$("#top_login_btn").addEventListener('mouseover', function () {
-    
+$("#top_login_btn").addEventListener("mouseover", function () {
+
 });
+
+// 删除按钮
+$(".search-del").addEventListener("click", function () {
+    $("#serch_song_key").value = "";
+})
+// 搜索功能实现
+$("#serch_song_key").addEventListener("input", function () {
+    // console.log("2");  //测试input
+
+    let key_value = this.value;
+    // console.log(key_value);
+
+    render("http://localhost:3000/search?keywords=" + key_value).then(data => {
+        addHtml($("#search_song_name"), data.result.songs, function callback(link, value, index) {
+            let a = "";
+            let b = "";
+            if (value.name == undefined) {
+                value.name = "";
+            }
+            switch (value.artists.length) {
+                case 1:
+                    a = value.artists[0].name;
+                    break;
+                case 2:
+                    a = value.artists[0].name;
+
+                    b = value.artists[1].name
+                    break;
+                default:
+                    break;
+            }
+            return input =
+                `
+                <a href="">
+                        <li>
+                            <div class="song-list-content-left first num">
+                            </div>
+                            <!-- 左边歌名部分 -->
+                            <div class="song-list-left hot-song-change">
+                                <p>${value.name}<span></span></p>
+                                <p>     
+                                    <!-- 下面图标 -->
+                                    <i></i>
+                                    <span>${a}</span>
+                                    <span>${b}-${value.album.name}</span>
+                                    <!-- 歌手 -->
+            
+                                </p>
+                            </div>
+                            <!-- 右边播放部分 -->
+                            <div class="radio-box">
+                                <i class="radio-btn"></i>
+                            </div>
+            
+                        </li>
+                    </a>
+                `
+        })
+    })
+    // 歌手
+    render("http://localhost:3000/search?keywords=" + key_value+"&type=100&limit=1").then(data => {
+        addHtml($(".search-key-song"), data.result.artists, function callback(link, value, index) {
+            return input =
+                `
+                <div class="search-song-list">
+                    <div>
+                        <img class="song-icon" src="../img/109951163111191410.webp" alt="">
+                        <p>
+                            歌手：
+                            <span>${value.name} ${value.alias}</span>
+                        </p>
+                    </div>
+                    <i>&gt;</i>
+                </div>
+                `
+        })
+    })
+    // 专辑
+    
+
+})
